eb7be466f02fa06d6b7d7cccf2461df67e72525a
pemrouz <p3pedram@hotmail.com>
2017-07-19 00:42:45 +0100

    feat: co-locate session creation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 // -------------------------------------------
 // Populates sessionID on each connection
 // -------------------------------------------
-export default function sessions(ripple, { session = {} } = {}){
+export default function(ripple, { session } = {}){
   log('creating')
-  const { secret, name } = session
-  if (!secret || !name) return ripple
-  ripple.io.use(auth(secret, name))
+  if (!session) return ripple
+  ripple.server.express
+    .use(cookies(session.secret))
+    .use(sessions(session))
+  ripple.io
+    .use(auth(session))
   return ripple
 }
 
-import cookie from 'cookie-parser'
+import sessions from 'express-session'
+import cookies from 'cookie-parser'
 import client from 'utilise/client'
 import noop from 'utilise/noop'
 import key from 'utilise/key'
 const log = require('utilise/log')('[ri/sessions]')
-    , auth = (secret, name) => (socket, next) => {
+    , auth = ({ secret, name }) => (socket, next) => {
         const req = {}
         key('headers.cookie', socket.request.headers.cookie)(req)
-        cookie(secret)(req, null, noop)
+        cookies(secret)(req, null, noop)
         socket.sessionID = req.signedCookies[name] || req.cookies[name]
         next()
       }
